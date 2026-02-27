4975c9881de2ad65cf863960ddd2007951a1c62b
Yiranubari <yiranubari4@gmail.com>
2026-02-27 08:36:37 +0100

    Add response header setup in server handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-type": "text/html" });
   res.end("This is the response from the server");
 });
 
