d3e5928f148c09b8f80882be5d622aab9f6aea3b
Yiranubari <yiranubari4@gmail.com>
2026-02-27 08:47:34 +0100

    Log parsed URL query in server handler

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
+  const query = url.parse(req.url, true);
+  console.log(query);
+
   res.writeHead(200, { "Content-type": "text/html" });
   res.end("This is the response from the server");
 });
